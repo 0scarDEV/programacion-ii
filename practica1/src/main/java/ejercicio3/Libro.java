7c998ce55fa4c644e2509b6434a0177741df0915
0scarDEV <oscarfdezptza94@gmail.com>
2026-02-04 17:30:14 +0100

    Función crear libro desde consola (xd)

--- practica1/src/main/java/ejercicio3/Libro.java
+++ practica1/src/main/java/ejercicio3/Libro.java
@@ -1,5 +1,7 @@
 package ejercicio3;
 
+import java.util.Scanner;
+
 public class Libro {
     String titulo;
     String[] autores;
@@ -13,6 +15,28 @@ public class Libro {
         this.editorial = editorial;
         this.anhoEdicion = anhoEdicion;
         this.isbn = isbn;
+    }
+
+    public static Libro crearLibroDesdeConsola() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el título: ");
+        String titulo = sc.nextLine();
+
+        System.out.println("Introduce los autores (separados por comas): ");
+        String[] autores = sc.nextLine().split(",");
+
+        System.out.println("Introduce la editorial: ");
+        String editorial = sc.nextLine();
+
+        System.out.println("Introduce el año de edición: ");
+        int anhoEdicion = sc.nextInt();
+        sc.nextLine(); // Consumir el salto de línea pendiente
+        
+        System.out.println("Introduce el ISBN: ");
+        String isbn = sc.nextLine();
+        sc.close();
+        return new Libro(titulo, autores, editorial, anhoEdicion, isbn);
     }
 
     @Override
