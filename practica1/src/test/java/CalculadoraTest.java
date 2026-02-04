2da410335eb008c3b64043d7ca1139d4dfc842ea
0scarDEV <oscarfdezptza94@gmail.com>
2026-02-04 17:40:12 +0100

    Calculadora test

---
+++ practica1/src/test/java/CalculadoraTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+import ejercicio4.Calculadora;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    @Test
+    void testSumarDos() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.sumar(2, 3);
+        assertEquals(5, resultado);
+    }
+
+    @Test
+    void testSumarTres() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.sumar(2, 3, 4);
+        assertEquals(9, resultado);
+    }
+
+    @Test
+    void testSumarDecimales() {
+        Calculadora calc = new Calculadora();
+        double resultado = calc.sumar(2.5, 3.5);
+        assertEquals(6.0, resultado);
+    }
+
+    @Test
+    void multiplicarEnteros() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.multiplicar(2, 3);
+        assertEquals(6, resultado);
+    }
+
+    @Test
+    void multiplicarEnteroDecimal() {
+        Calculadora calc = new Calculadora();
+        double resultado = calc.multiplicar(2, 2.5);
+        assertEquals(5.0, resultado);
+    }
+
+    @Test
+    void descuentoPorcentual() {
+        Calculadora calc = new Calculadora();
+        double resultado = calc.getPrecioTrasDescuentoPorcentual(200.0, 10.0);
+        assertEquals(180.0, resultado);
+    }
+
+    @Test
+    void descuentoFijo() {
+        Calculadora calc = new Calculadora();
+        double resultado = calc.getPrecioTrasDescuentoFijo(200.0, 30);
+        assertEquals(170.0, resultado);
+    }
+    
+    @Test
+    void descuentoFijoConTope() {
+        Calculadora calc = new Calculadora();
+        double resultado = calc.getPrecioTrasDescuentoFijo(200.0, 25.0, 20.0);
+        assertEquals(160.0, resultado);
+    }
+}
