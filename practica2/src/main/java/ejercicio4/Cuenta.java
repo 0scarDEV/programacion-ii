6d98396408233ec127fea4fcd3ffa445003bee8d
0scarDEV <oscarfdezptza94@gmail.com>
2026-02-11 16:48:47 +0100

    System.exit(1) -- Supongamos

--- practica2/src/main/java/ejercicio4/Cuenta.java
+++ practica2/src/main/java/ejercicio4/Cuenta.java
@@ -15,6 +15,7 @@ public class Cuenta {
         }
     }
 
+    /*
     public boolean ingresar(double cantidad) {
         if (cantidad > 0) {
             saldo += cantidad;
@@ -29,5 +30,21 @@ public class Cuenta {
             return true;
         }
         return false;
+    } */
+
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        } else {
+            System.exit(1);
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+        } else {
+            System.exit(1);
+        }
     }
 }
