7f1da58beeb713d41e0c4466ce2d622a6a8b420e
0scarDEV <oscarfdezptza94@gmail.com>
2026-02-11 16:35:30 +0100

    Practica2.Ejercicio3Test

---
+++ practica2/src/test/java/TipoIVATest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ejercicio3.TipoIVA;
+
+class TipoIVATest {
+    @Test
+    void generalDebeAplicar21PorCiento() {
+        double resultado = TipoIVA.GENERAL.aplicarIVA(100.0);
+        Assertions.assertEquals(121.0, resultado, 0.0001);
+    }
+
+    @Test
+    void exentoNoDebeModificarPrecio() {
+        double resultado = TipoIVA.EXENTO.aplicarIVA(100.0);
+        Assertions.assertEquals(100.0, resultado, 0.0001);
+    }
+}
