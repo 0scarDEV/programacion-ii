61d0d898cb59edf8c145c736b17592cbb857ee03
0scarDEV <oscarfdezptza94@gmail.com>
2026-02-25 16:33:46 +0100

    P2E6 Modificada encapsulacion

--- practica2/src/main/java/ejercicio6/NumeroComplejo.java
+++ practica2/src/main/java/ejercicio6/NumeroComplejo.java
@@ -1,24 +1,29 @@
 package ejercicio6;
 
 public class NumeroComplejo {
-    double pReal;
-    double pImaginaria;
+    double[] componentes = new double[2];
+    //double pReal;
+    //double pImaginaria;
 
     public NumeroComplejo() {
         this(0,0);
     }
 
     public NumeroComplejo(double pReal, double pImaginaria) {
-        this.pReal = pReal;
-        this.pImaginaria = pImaginaria;
+        //this.pReal = pReal;
+        //this.pImaginaria = pImaginaria;
+        this.componentes[0] = pReal;
+        this.componentes[1] = pImaginaria;
     }
 
     public double getpReal() {
-        return pReal;
+        //return pReal;
+        return componentes[0];
     }
 
     public double getpImaginaria() {
-        return pImaginaria;
+        //return pImaginaria;
+        return componentes[1];
     }
 
     public static NumeroComplejo suma(NumeroComplejo a, NumeroComplejo b) {
@@ -30,8 +35,10 @@ public class NumeroComplejo {
     }
 
     public NumeroComplejo getAbsValue() {
-        double absReal = Math.abs(this.pReal);
-        double absImaginaria = Math.abs(this.pImaginaria);
+        //double absReal = Math.abs(this.pReal);
+        //double absImaginaria = Math.abs(this.pImaginaria);
+        double absReal = Math.abs(this.componentes[0]);
+        double absImaginaria = Math.abs(this.componentes[1]);
         return new NumeroComplejo(absReal, absImaginaria);
     }
 }
