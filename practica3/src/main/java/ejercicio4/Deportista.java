bb503d40aa37718ccab5c10f200d14c7fde4d55e
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-04 16:08:56 +0100

    P3E4Clase

---
+++ practica3/src/main/java/ejercicio4/Deportista.java
@@ -0,0 +1,41 @@
+package ejercicio4;
+
+public class Deportista {
+    public enum Categoria {
+        BENJAMIN, ALEVIN, INFANTIL, CADETE, JUVENIL, SENIOR;
+    }
+
+    private String dni;
+    private String nombre;
+    private int edad;
+    private Categoria categoria;
+
+    public Deportista(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        if (edad < 0) {
+            throw new IllegalArgumentException("La edad no puede ser negativa");
+        }
+        this.edad = edad;
+    }
+
+    public String getDni() {return dni;}
+    public String getNombre() {return nombre;}
+    public int getEdad() {return edad;}
+    public Categoria getCategoria() {
+        if (edad <= 8) {
+            categoria = Categoria.BENJAMIN;
+        } else if (edad <= 10) {
+            categoria = Categoria.ALEVIN;
+        } else if (edad <= 12) {
+            categoria = Categoria.INFANTIL;
+        } else if (edad <= 14) {
+            categoria = Categoria.CADETE;
+        } else if (edad <= 18) {
+            categoria = Categoria.JUVENIL;
+        } else {
+            categoria = Categoria.SENIOR;
+        }
+        return categoria;
+    }
+}
