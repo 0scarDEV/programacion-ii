465b9f6cd0efb53843c8880c2dec4f1b2c3346a9
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-04 16:29:24 +0100

    P3E1. Ciertas correcciones

--- practica3/src/main/java/ejercicio1/Division.java
+++ practica3/src/main/java/ejercicio1/Division.java
@@ -6,44 +6,44 @@ public class Division {
     public static int divide(int a, int b) {
         try {
             return a / b;
-        } catch (java.lang.ArithmeticException e) {
+        } catch (ArithmeticException e) {
             System.out.println("\nError: No se puede dividir por cero.");
             return 0;
         }
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int num1 = 0;
         int num2 = 0;
         String linea;
-        Scanner scan = new Scanner(System.in);
         boolean flagDividendo = false;
         boolean flagDivisor = false;
 
         do {
             System.out.print("\nIntroduzca dividendo: ");
-            linea = scan.nextLine();
+            linea = sc.nextLine();
             try {
                 num1 = Integer.parseInt(linea);
                 flagDividendo = true;
             } catch (NumberFormatException e) {
-                System.out.println("\nError: Entrada no válida. Por favor, introduzca un número entero.");
+                System.out.println("\nError: Entrada no válida para el dividendo. Por favor, introduzca un número entero.");
             }
         } while (!flagDividendo);
 
         do {
             System.out.print("\nIntroduzca divisor: ");
-            linea = scan.nextLine();
+            linea = sc.nextLine();
             try {
                 num2 = Integer.parseInt(linea);
                 flagDivisor = true;
             } catch (NumberFormatException e) {
-                System.out.println("\nError: Entrada no válida. Por favor, introduzca un número entero.");
+                System.out.println("\nError: Entrada no válida para el divisor. Por favor, introduzca un número entero.");
             }
         } while (!flagDivisor);
 
         System.out.println("\nEl resultado es: " + divide(num1, num2));
 
-        scan.close();
+        sc.close();
     }
 }
