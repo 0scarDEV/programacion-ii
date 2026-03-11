3c825cba08aabea2b64977d5b3d90ac0b917275a
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-11 17:46:48 +0100

    P2E6. assert por assertEquals

--- practica2/src/test/java/ejercicio6/ComplejoTest.java
+++ practica2/src/test/java/ejercicio6/ComplejoTest.java
@@ -1,4 +1,6 @@
 package ejercicio6;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ComplejoTest {
@@ -7,8 +9,8 @@ public class ComplejoTest {
         NumeroComplejo a = new NumeroComplejo(1, 2);
         NumeroComplejo b = new NumeroComplejo(3, 4);
         NumeroComplejo resultado = NumeroComplejo.suma(a, b);
-        assert resultado.getpReal() == 4;
-        assert resultado.getpImaginaria() == 6;
+        assertEquals(resultado.getpReal(), 4);
+        assertEquals(resultado.getpImaginaria(), 6);
     }
 
     @Test
@@ -16,47 +18,47 @@ public class ComplejoTest {
         NumeroComplejo c = new NumeroComplejo(2, 3);
         double d = 4;
         NumeroComplejo resultado = NumeroComplejo.multiplicacion(c, d);
-        assert resultado.getpReal() == 8;
-        assert resultado.getpImaginaria() == 12;
+        assertEquals(resultado.getpReal(), 8);
+        assertEquals(resultado.getpImaginaria(), 12);
     }
 
     @Test
     public void testAbsValue() {
         NumeroComplejo e = new NumeroComplejo(-5, -6);
         NumeroComplejo resultado = e.getAbsValue();
-        assert resultado.getpReal() == 5;
-        assert resultado.getpImaginaria() == 6;
+        assertEquals(resultado.getpReal(), 5);
+        assertEquals(resultado.getpImaginaria(), 6);
     }
 
     @Test
     public void testCERO() {
-        assert NumeroComplejo.CERO.getpReal() == 0;
-        assert NumeroComplejo.CERO.getpImaginaria() == 0;
+        assertEquals(NumeroComplejo.CERO.getpReal(), 0);
+        assertEquals(NumeroComplejo.CERO.getpImaginaria(), 0);
     }
 
     @Test
     public void testUNO() {
-        assert NumeroComplejo.UNO.getpReal() == 1;
-        assert NumeroComplejo.UNO.getpImaginaria() == 0;
+        assertEquals(NumeroComplejo.UNO.getpReal(), 1);
+        assertEquals(NumeroComplejo.UNO.getpImaginaria(), 0);
     }
 
     @Test
     public void testI() {
-        assert NumeroComplejo.I.getpReal() == 0;
-        assert NumeroComplejo.I.getpImaginaria() == 1;
+        assertEquals(NumeroComplejo.I.getpReal(), 0);
+        assertEquals(NumeroComplejo.I.getpImaginaria(), 1);
     }
 
     @Test
     public void testReal() {
         NumeroComplejo real = NumeroComplejo.real(5);
-        assert real.getpReal() == 5;
-        assert real.getpImaginaria() == 0;
+        assertEquals(real.getpReal(), 5);
+        assertEquals(real.getpImaginaria(), 0);
     }
 
     @Test
     public void testImaginario() {
         NumeroComplejo imaginario = NumeroComplejo.imaginario(7);
-        assert imaginario.getpReal() == 0;
-        assert imaginario.getpImaginaria() == 7;
+        assertEquals(imaginario.getpReal(), 0);
+        assertEquals(imaginario.getpImaginaria(), 7);
     }
 }
