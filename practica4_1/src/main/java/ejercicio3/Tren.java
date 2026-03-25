3b2d00a65f8cca72bd5d6dd4681e0424182eab04
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-25 17:12:45 +0100

    P4E3T1

---
+++ practica4_1/src/main/java/ejercicio3/Tren.java
@@ -0,0 +1,29 @@
+package ejercicio3;
+
+public class Tren {
+    private int cvPotencia;
+    private Vagon primerVagon;
+
+    public Tren(int cvPotencia, Vagon primerVagon) {
+        this.cvPotencia = cvPotencia;
+        this.primerVagon = primerVagon;
+    }
+
+    public int getCvPotencia() {
+        return cvPotencia;
+    }
+
+    public void setCvPotencia(int cvPotencia) {
+        this.cvPotencia = cvPotencia;
+    }
+
+    public Vagon getPrimerVagon() {
+        return primerVagon;
+    }
+
+    public void setPrimerVagon(Vagon primerVagon) {
+        this.primerVagon = primerVagon;
+    }
+
+    
+}
