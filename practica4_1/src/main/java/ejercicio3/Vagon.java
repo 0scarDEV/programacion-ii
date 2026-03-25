7ee154709377196d22fcd6771cc81be309afce7d
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-25 17:13:02 +0100

    P4E3T2

--- practica4_1/src/main/java/ejercicio3/Vagon.java
+++ practica4_1/src/main/java/ejercicio3/Vagon.java
@@ -1,7 +1,26 @@
 package ejercicio3;
 
 public class Vagon {
-    
+    private Vagon vagonAnterior;
+    private Vagon vagonSiguiente;
 
-    
+    public Vagon(Vagon vagonAnterior, Vagon vagonSiguiente) {
+        this.vagonAnterior = vagonAnterior;
+        this.vagonSiguiente = vagonSiguiente;
+    }
+
+    public Vagon getVagonAnterior() {
+        return vagonAnterior;
+    }
+
+    public Vagon getVagonSiguiente() {
+        return vagonSiguiente;
+    }
+
+    public void setVagonSiguiente(Vagon vagonSiguiente) {
+        if (this.vagonSiguiente != null) {
+            this.vagonSiguiente.vagonAnterior = null; // Desconectar el vagon siguiente actual
+        }
+        this.vagonSiguiente = vagonSiguiente;
+    }
 }
