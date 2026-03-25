c6d6415b0a56ce3bd835e582b6b19582d9f46e42
0scarDEV <oscarfdezptza94@gmail.com>
2026-03-25 16:37:54 +0100

    P4E2

---
+++ practica4_1/src/main/java/ejercicio2/Reunion.java
@@ -0,0 +1,45 @@
+package ejercicio2;
+import ejercicio1.Fecha;
+import ejercicio1.Hora;
+
+public class Reunion {
+    private String asunto;
+    private String lugar;
+    private Fecha fecha;
+    private Hora horaCelebracion;
+
+    public Reunion(String asunto, String lugar, int anho, int mes, int dia, int hora, int minuto) {
+        setAsunto(asunto);
+        setLugar(lugar);
+        
+        // Composición fuerte
+        setFecha(new Fecha(dia, mes, anho));
+        setHoraCelebracion(new Hora(hora, minuto));
+    }
+
+    // Getters
+    public String getAsunto() { return asunto; }
+    public String getLugar() { return lugar; }
+    public Fecha getFecha() {  // Composición fuerte
+        return new Fecha(fecha.getDia(), fecha.getMes(), fecha.getAnho()); 
+    }
+    public Hora getHoraCelebracion() {  // Composición fuerte
+        return new Hora(horaCelebracion.getHora(), horaCelebracion.getMinuto()); 
+    }
+
+    // Setters
+    public void setAsunto(String asunto) { this.asunto = asunto; }
+    public void setLugar(String lugar) { this.lugar = lugar; }
+    public void setFecha(Fecha fecha) {
+        if (fecha == null) {
+            throw new IllegalArgumentException("La fecha no puede ser nula");
+        }
+        this.fecha = fecha;
+    }
+    public void setHoraCelebracion(Hora horaCelebracion) {
+        if (horaCelebracion == null) {
+            throw new IllegalArgumentException("La hora de celebración no puede ser nula");
+        }
+        this.horaCelebracion = horaCelebracion;
+    }
+}
