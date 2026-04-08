fbefba09c0b4a38cc9c41c8a6aa53f5381befb3e
0scarDEV <oscarfdezptza94@gmail.com>
2026-04-08 16:44:56 +0200

    P4.2E3T4

--- practica4_2/src/main/java/principal/AplicacionPublicaciones.java
+++ practica4_2/src/main/java/principal/AplicacionPublicaciones.java
@@ -2,6 +2,8 @@ package principal;
 
 import ejercicio1.Autor;
 import ejercicio2.*;
+import ejercicio3.RegistroPublicacionesAutor;
+
 import java.util.ArrayList;
 
 public class AplicacionPublicaciones {
@@ -11,12 +13,15 @@ public class AplicacionPublicaciones {
         Autor autor2 = new Autor("2", "Desconocido", "Autor", "InstitutoY");
         Autor autor3 = new Autor("3", "Desconocido", "Autor", "InstitutoZ");
 
-        publicaciones.add(new Libro("El Quijote", 1605, 0, autor1, "Anaya", "Madrid"));
-        publicaciones.add(new ComunicacionEnCongreso("Titulo", 2010, 0, autor2, "CongresoX", "CiudadY", "PaisZ"));
-        publicaciones.add(new Articulo("Articulo1", 2020, 0, autor3, "Revista1", 1, 1, 1));
+        publicaciones.add(new Libro("El Quijote", 1605, 2, autor1, "Anaya", "Madrid"));
+        publicaciones.add(new ComunicacionEnCongreso("Titulo", 2010, 3, autor2, "CongresoX", "CiudadY", "PaisZ"));
+        publicaciones.add(new Articulo("Articulo1", 2020, 10, autor3, "Revista1", 1, 1, 1));
 
         for (Publicacion pub : publicaciones) {
             System.out.print("\n" + pub);
         }
+
+        RegistroPublicacionesAutor registro = new RegistroPublicacionesAutor(autor1, publicaciones);
+        System.out.println("\nÍndice C del autor " + autor1.getNombre() + " " + autor1.getApellidos() + ": " + registro.calcularIndiceC());
     }
 }
