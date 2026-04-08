f3264cdc32424323f4d2d9ab59f05555756dbd48
0scarDEV <oscarfdezptza94@gmail.com>
2026-04-08 16:40:34 +0200

    P4.2E3T2

--- practica4_2/src/main/java/ejercicio3/RegistroPublicacionesAutor.java
+++ practica4_2/src/main/java/ejercicio3/RegistroPublicacionesAutor.java
@@ -3,6 +3,9 @@ package ejercicio3;
 import java.util.List;
 
 import ejercicio1.Autor;
+import ejercicio2.Articulo;
+import ejercicio2.ComunicacionEnCongreso;
+import ejercicio2.Libro;
 import ejercicio2.Publicacion;
 
 public class RegistroPublicacionesAutor {
@@ -24,5 +27,23 @@ public class RegistroPublicacionesAutor {
 
     public void addPublicacion(Publicacion publicacion) {
         publicaciones.add(publicacion);
+    }
+
+    public double calcularIndiceC() {
+        double numCitasArticulo = 0;
+        double numCitasLibro = 0;
+        double numCitasComunicacion = 0;
+
+        for (Publicacion pub : publicaciones) {
+            if (pub instanceof Articulo) {
+                numCitasArticulo += pub.getNumCitas();
+            } else if (pub instanceof Libro) {
+                numCitasLibro += pub.getNumCitas();
+            } else if (pub instanceof ComunicacionEnCongreso) {
+                numCitasComunicacion += pub.getNumCitas();
+            }
+        }
+
+        return (numCitasArticulo * 1) + (numCitasLibro * 0.8) + (numCitasComunicacion * 0.6);
     }
 }
