7175f4dc99ce65eb77a8807eb74f45fb85e14dab
0scarDEV <oscarfdezptza94@gmail.com>
2026-05-06 16:16:22 +0200

    P6E1T2

--- practica6/src/main/java/ejercicio1/UsandoLambdas.java
+++ practica6/src/main/java/ejercicio1/UsandoLambdas.java
@@ -2,6 +2,7 @@ package ejercicio1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UsandoLambdas {
     public static void main(String[] args) {
@@ -12,5 +13,21 @@ public class UsandoLambdas {
         lista.add("tres");
 
         lista.forEach(s -> System.out.println(s));
-    }
+
+        AtomicInteger longitudTotal = new AtomicInteger(0);
+        int[] longitudTotalArray = {0};
+        List<String> losDeTamañoMayorA3 = new ArrayList<>();
+        lista.forEach(s -> {
+            if (s.length() > 3) {
+                losDeTamañoMayorA3.add(s);
+            }
+            longitudTotal.set(longitudTotal.get() + s.length());
+            longitudTotalArray[0] += s.length();
+        });
+        System.out.println("Longitud total: " + longitudTotal.get());
+        System.out.println("Longitud total (array): " + longitudTotalArray[0]);
+
+        // AtomicInteger funciona por ser un Objeto mutable.
+        // El array funciona porque el array es un objeto mutable, aunque el array en sí no se modifique, su contenido sí puede ser modificado.    }
+        
 }
