298e4b6549b090af733ef4d3e03b7b30eff1736d
0scarDEV <oscarfdezptza94@gmail.com>
2026-05-06 16:09:19 +0200

    P6E1

---
+++ practica6/src/main/java/ejercicio1/UsandoLambdas.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsandoLambdas {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+
+        lista.add("uno");      
+        lista.add("dos");
+        lista.add("tres");
+
+    }
+}
