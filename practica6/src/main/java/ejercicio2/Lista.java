e8ddc2fb26ddc9bd6b5202cd30b829c6d29ee101
0scarDEV <oscarfdezptza94@gmail.com>
2026-05-06 16:26:02 +0200

    P6E2

---
+++ practica6/src/main/java/ejercicio2/Lista.java
@@ -0,0 +1,56 @@
+package ejercicio2;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+class Lista<T> {
+    private T[] elementos;
+    private int numElementos = 0;
+
+    public Lista(int tamañoMaximo) {
+        this.elementos = (T[]) new Object[tamañoMaximo];
+    }
+
+    public void añadir(T elem) {
+        if (this.numElementos == elementos.length) {
+            throw new IllegalArgumentException("Lista llena");
+        }
+        this.elementos[numElementos++] = elem;
+    }
+
+    public T obtener(int pos) {
+        validarPosicion(pos);
+
+        return this.elementos[pos];
+    }
+
+    public void eliminar(int pos) {
+        validarPosicion(pos);
+
+        for (int i = pos; i < this.numElementos - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+        }
+        this.numElementos--;
+    }
+
+    private void validarPosicion(int pos) throws IllegalArgumentException {
+        if (pos < 0 || pos >= this.numElementos) {
+            throw new IllegalArgumentException("eliminar: posicion no valida: " + pos);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder toString = new StringBuilder();
+        toString.append("[");
+        for (int i = 0; i < this.numElementos; i++) {
+            if (i > 0) {
+                toString.append(", ");
+            }
+            toString.append(this.elementos[i].toString());
+        }
+        toString.append("]");
+        return toString.toString();
+    }
+}
